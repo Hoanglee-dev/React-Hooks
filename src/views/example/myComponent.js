41db2cb2ea1b5e9522cf096ee1208b521cc629aa
Hoanglee-dev <levuhoangks@gmail.com>
2024-09-16 10:41:18 +0700

    Split Components

--- src/views/example/myComponent.js
+++ src/views/example/myComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ChildComponent from "./ChildComponent";
+import AddComponent from "./AddComponent";
 class MyComponent extends React.Component {
   /* 
   JSX: giúp js trả ra 1 đống html
@@ -7,8 +8,6 @@ class MyComponent extends React.Component {
   state: bản chất của nó là khi thay đổi dữ liệu, không cần reload lại page 
   */
   state = {
-    firstName: "",
-    lastName: "",
     arrJob: [
       { id: "abcjob1", title: "dev", salary: " 500" },
       { id: "abcjob2", title: "tester", salary: " 400" },
@@ -16,46 +15,13 @@ class MyComponent extends React.Component {
     ]
   };
 
-
-  handleChangeFirstName = (event) => {
-    this.setState({
-      firstName: event.target.value,
-    })
-  }
-  handleChangeLastName = (event) => {
-    this.setState({
-      lastName: event.target.value,
-    })
-  }
-  handleSubmit = (event) => {
-    event.preventDefault()
-    console.log("check data state", this.state)
-  }
   render() {
     return (
       <>
-        <form >
-          <label htmlFor="fname">First name:</label><br />
-          <input
-            type="text"
-            value={this.state.firstName}
-            onChange={(event) => this.handleChangeFirstName(event)}
-          /><br />
-          <label htmlFor="lname">Last name:</label><br />
-          <input
-            type="text"
-            value={this.state.lastName}
-            onChange={(event) => this.handleChangeLastName(event)}
-          /><br /><br />
-          <input type="submit"
-            value="Submit"
-            onClick={(event) => this.handleSubmit(event)}
-          />
-        </form>
+        <AddComponent />
+
         <ChildComponent
-          name={this.state.firstName}
-          age={"24"}
-          address={"DaNang"}
+
           arrJob={this.state.arrJob}
         />
       </>
